bdfe147c985771ffedffd2125e91a74ae3f03914
Luke Stauffenger <lucas.miranda96@hotmail.com>
2020-03-09 04:40:52 -0300

    Implementação da instalação dos repositórios e dos programas.

--- main.sh
+++ main.sh
@@ -53,6 +53,105 @@ function select_display_manager () {
     esac
 }
 
+function install_repositories () {
+    local repository="$1"
+    case repository in
+        basic)
+            #rpm fusion
+            dnf install -yq https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+            #flatpak
+            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            ;;
+        dev)
+            #postgress
+            dnf install -yq https://download.postgresql.org/pub/repos/yum/reporpms/F-31-x86_64/pgdg-fedora-repo-latest.noarch.rpm;
+
+            #sublime text
+            rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+            dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
+            #vscode
+            rpm --import https://packages.microsoft.com/keys/microsoft.asc
+            sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+            ;;
+        social)
+            ;;
+        all)
+            install_repositories dev
+            install_repositories social
+            ;;
+    esac
+    dnf check-update;
+}
+
+function install_programs () {
+    local programs="$1"
+    case programs in
+        basic)
+            dnf install wget -yq
+            dnf install -yq flatpak stacer system-config-language numlockx krita vlc* gimp
+            #Iniciando download de Google Chrome
+            wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+            dnf install -yq google-chrome-stable_current_x86_64.rpm
+
+            #Instalando programas via flatpak
+            flatpak install -yq flathub com.wps.Office --noninteractive
+            ;;
+        dev)
+            #Instalando programas via dnf
+            dnf install -yq code nano rabbitvcs* sublime-text codeblocks pgadmin4 blender npm golang
+            dnf groupinstall -yq 'PostgreSQL Database Server 10 PGDG' --with-optional
+
+            echo 'Inicializando a configuração Postgres 10'
+            /usr/pgsql-10/bin/postgresql-10-setup initdb
+            systemctl enable postgresql-10
+            systemctl start postgresql-10
+            systemctl start httpd 
+            systemctl enable httpd
+            cp ./exempl /etc/httpd/conf.d/pgadmin4.conf
+            systemctl restart httpd
+            mkdir -p /var/lib/pgadmin4/ /var/log/pgadmin4/
+            echo "LOG_FILE = '/var/log/pgadmin4/pgadmin4.log'
+            SQLITE_PATH = '/var/lib/pgadmin4/pgadmin4.db'
+            SESSION_DB_PATH = '/var/lib/pgadmin4/sessions'
+            STORAGE_DIR = '/var/lib/pgadmin4/storage'" >> /usr/lib/python3.7/site-packages/pgadmin4-web/config_distro.py
+            python3 /usr/lib/python3.7/site-packages/pgadmin4-web/setup.py
+            chown -R apache:apache /var/lib/pgadmin4 /var/log/pgadmin4
+            semanage fcontext -a -t httpd_sys_rw_content_t "/var/lib/pgadmin4(/.*)?"
+            semanage fcontext -a -t httpd_sys_rw_content_t "/var/log/pgadmin4(/.*)?"
+            restorecon -R /var/lib/pgadmin4/
+            restorecon -R /var/log/pgadmin4/
+            systemctl restart httpd
+            firewall-cmd --permanent --add-service=http
+            firewall-cmd --reload
+            
+            #Instalando programas via npm
+            npm i -g npm
+            npm i -g yarn
+
+            #Instalando programas via flatpak
+            flatpak install -yq flathub com.google.AndroidStudio --noninteractive
+            ;;
+        social)
+            dnf install -yq telegram-desktop steam*
+            #Iniciando download do Mega e do Mega Nemo extension
+            wget -c https://mega.nz/linux/MEGAsync/Fedora_31/x86_64/megasync-Fedora_31.x86_64.rpm
+            wget -c https://mega.nz/linux/MEGAsync/Fedora_31/x86_64/nemo-megasync-Fedora_31.x86_64.rpm
+            dnf install -yq megasync-Fedora_31.x86_64.rpm nemo-megasync-Fedora_31.x86_64.rpm
+
+            #Instalando programas via flatpak
+            flatpak install -yq flathub com.spotify.Client --noninteractive
+            flatpak install -yq flathub org.DolphinEmu.dolphin-emu --noninteractive
+            flatpak install -yq flathub com.discordapp.Discord --noninteractive
+            ;;
+        all)
+            install_programs dev
+            install_programs social
+            ;;
+    esac
+}
+
 #Tratando argumentos recebidos
 if [ $# ]; then
     if [ "$1" == "--only" ]; then
@@ -63,7 +162,8 @@ if [ $# ]; then
             exit 1
         fi
     else
-        echo 'do something'
+        install_repositories basic
+        install_programs basic
     fi
 fi
 
@@ -98,11 +198,9 @@ while [ $# -gt 0 ]; do
             if [ "$2" ]; then
                 local optionGroup="$2"
                 case $optionGroup in
-                    dev) 
-                        echo 'dev'
-                        ;;
-                    social) 
-                        echo 'social'
+                    dev | social)
+                        install_repositories ${optionGroup}
+                        install_programs ${optionGroup}
                         ;;
                     *)
                         error_message invalid ${1}
@@ -111,7 +209,8 @@ while [ $# -gt 0 ]; do
                 esac
                 shift
             else
-                echo 'All optionals things'
+                install_repositories all
+                install_programs all
             fi
             ;;
         -g | --gpu)
