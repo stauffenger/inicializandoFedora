573715b8020cb1b526309b39cfdc9266f495241e
Matheus Abreu <abreu@abreu.lan>
2020-06-30 21:37:24 -0300

    Add conteudo bashrc a variavel e excluindo arquivo exempl

--- index.sh
+++ index.sh
@@ -1,5 +1,69 @@
 #!/bin/bash
 
+bashrc='#!/usr/bin/env bash
+
+# If not running interactively, dont do anything
+case $- in
+  *i*) ;;
+    *) return;;
+esac
+
+# Path to the bash it configuration
+export BASH_IT="/home/$USER/.bash-it"
+
+# Lock and Load a custom theme file.
+# Leave empty to disable theming.
+# location /.bash_it/themes/
+export BASH_IT_THEME="powerline"
+
+# (Advanced): Change this to the name of your remote repo if you
+# cloned bash-it with a remote other than origin such as `bash-it`.
+# export BASH_IT_REMOTE="bash-it"
+
+# Your place for hosting Git repos. I use this for private repos.
+export GIT_HOSTING="git@git.domain.com"
+
+# Dont check mail when opening terminal.
+unset MAILCHECK
+
+# Change this to your console based IRC client of choice.
+export IRC_CLIENT="irssi"
+
+# Set this to the command you use for todo.txt-cli
+export TODO="t"
+
+# Set this to false to turn off version control status checking within the prompt for all themes
+export SCM_CHECK=true
+
+# Set Xterm/screen/Tmux title with only a short hostname.
+# Uncomment this (or set SHORT_HOSTNAME to something else),
+# Will otherwise fall back on $HOSTNAME.
+#export SHORT_HOSTNAME=$(hostname -s)
+
+# Set Xterm/screen/Tmux title with only a short username.
+# Uncomment this (or set SHORT_USER to something else),
+# Will otherwise fall back on $USER.
+#export SHORT_USER=${USER:0:8}
+
+# Set Xterm/screen/Tmux title with shortened command and directory.
+# Uncomment this to set.
+#export SHORT_TERM_LINE=true
+
+# Set vcprompt executable path for scm advance info in prompt (demula theme)
+# https://github.com/djl/vcprompt
+#export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
+
+# (Advanced): Uncomment this to make Bash-it reload itself automatically
+# after enabling or disabling aliases, plugins, and completions.
+# export BASH_IT_AUTOMATIC_RELOAD_AFTER_CONFIG_CHANGE=1
+
+# Uncomment this to make Bash-it create alias reload.
+# export BASH_IT_RELOAD_LEGACY=1
+
+# Load Bash It
+source "$BASH_IT"/bash_it.sh
+';
+
 dnf install wget -y;
 
 #add repos
@@ -75,7 +139,7 @@ echo 'Instalando Tema Terminal';
 git clone https://github.com/Bash-it/bash-it.git /home/$USER/.bash-it/;
 chmod +x /home/$USER/.bash-it/install.sh;
 echo 'y' | sh /home/$USER/.bash-it/install.sh;
-cp ./bashrc /home/$USER/.bashrc ;
+cp $bashrc /home/$USER/.bashrc ;
 cp /home/$USER/.bashrc /root/.bashrc ;
 
 echo 'Configurando VS Code';
