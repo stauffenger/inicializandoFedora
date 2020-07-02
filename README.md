ebcf31c75591a3a1fefc385cac01a662d7ec78cc
Matheus Abreu <abreu@abreu.lan>
2020-07-02 00:09:39 -0300

    Seperando scrpits de intalação e scrpit de configuração Editando do README

--- README.md
+++ README.md
@@ -1,34 +1,58 @@
 # inicializandoFedora
-um shell script que já configura tudo que preciso no fedora.
 
-Após a execução do script executar o comando abaixo, selecionar português(Brasil) e clicar em 'ok'.
+Um shell script que já configura tudo que preciso no fedora.
+Ele é bem pessoal, então sugiro que antes de rodar dê um molhada no que ele instalando.
+
+## Como Executar:
+
 ```shell
+# Executar sem superusuario. O script esta encarregado de elevar-se (Por isso fica atento, ele pedirá senha)
+$ ./index.sh
+
+# Após a execução do script executar o comando abaixo, selecionar português(Brasil) e clicar em 'ok'.
 $ system-config-language
 ```
 
-## Configurando vscode
-* VS Code sendo configurado automaticamente
-	* Tema
-		* Ayu Mirage
-	* Fonte
-		* Fira Code Retina
-	* Extensões 
-		
-```shell
-$ code --install-extension teabyii.ayu dbaeumer.vscode-eslint rvest.vs-code-prettier-eslint ms-python.python
+### Configurando tema do bash
 
-$ echo '{ 
-	"workbench.iconTheme": "ayu",
-	 "workbench.colorTheme": "Ayu Mirage",
-	  "editor.fontFamily": "Fira Code Retina",
-	  "editor.formatOnType": true,
-	  "editor.fontLigatures": true,
-	  "explorer.confirmDelete": false,
-	  "editor.formatOnSave": true,
-	  "prettier.eslintIntegration": true
-	  }' > /home/$USER/.config/Code/User/settings.json
+Os temas aplicaveis no bash são os temas presentes na [Wiki](https://github.com/Bash-it/bash-it/wiki/Themes), é uma lista extensa de temas possiveis.
+Basta mudar o valor da variável yourTheme para o nome do tema que deseja, e o script se encarregará do resto.
+
+### Configurando vscode
+
+- VS Code sendo configurado automaticamente com:
+  - Tema:
+    - Ayu Mirage
+  - Fonte:
+    - Fira Code Retina
+  - Extensões:
+    - EsLint (dbaeumer.vscode-eslint)
+    - Prettier (esbenp.prettier-vscode)
+    - Python (ms-python.python)
+    - Color Highlight (naumovs.color-highlight)
+
+> Configuração no settings.json
+
+```json
+{
+  "workbench.iconTheme": "vscode-icons",
+  "workbench.colorTheme": "Ayu Mirage",
+  "editor.fontFamily": "Fira Code Retina",
+  "editor.formatOnType": true,
+  "explorer.compactFolders": false,
+  "editor.fontLigatures": true,
+  "explorer.confirmDelete": false,
+  "editor.formatOnSave": true,
+  "prettier.eslintIntegration": true,
+  "prettier.singleQuote": true,
+  "[javascript]": { "editor.defaultFormatter": "esbenp.prettier-vscode" }
+}
 ```
-## Configurando Subllime Text
+
+### Configurando Subllime Text
+
+A Configuração do Sublime Text é manual.
+
 > Instale primeiramente o Package Control e o tema Ayu pelo Package Control
 
 ```shell
@@ -45,4 +69,8 @@ $ echo '{
 }' > /home/$USER/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ```
 
->[Documentação do Postgres](https://www.postgresql.org/download/linux/redhat/, 'https://www.postgresql.org/') pro Fedora e a [Documentação de configuração do Fedora](https://fedoraproject.org/wiki/PostgreSQL#Configuration, 'https://fedoraproject.org/wiki/PostgreSQL#Configuration') e um link que me ajudou a configurar o [pgadmin4](https://computingforgeeks.com/how-to-install-pgadmin-on-centos-fedora/, 'https://computingforgeeks.com/how-to-install-pgadmin-on-centos-fedora/')
+### Configurando o Postgres
+
+Particulamente eu gosto de seguir a [Documentação de configuração do Fedora](https://fedoraproject.org/wiki/PostgreSQL#Configuration, 'https://fedoraproject.org/wiki/PostgreSQL#Configuration') para configurar o postgresql.
+
+Contudo como material complementar também existe a [Documentação do Postgres](https://www.postgresql.org/download/linux/redhat/, 'https://www.postgresql.org/').
--- index.sh
+++ index.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#As variveis yourTheme, yourGitRepoPriv, yourIRC devem estar dentro de aspas duplas pois as aspas simples precisam entrar na confguração do arquivo
+
+iAm=$USER
+
+yourTheme="'powerline'";
+
+yourGitRepoPriv="'git@git.domain.com'";
+
+yourIRC="'irssi'";
 
 bashrc='#!/usr/bin/env bash
 
@@ -9,25 +19,21 @@ case $- in
 esac
 
 # Path to the bash it configuration
-export BASH_IT="/home/$USER/.bash-it"
+export BASH_IT="/home/'$USER'/.bash-it"
 
 # Lock and Load a custom theme file.
 # Leave empty to disable theming.
 # location /.bash_it/themes/
-export BASH_IT_THEME="powerline"
-
-# (Advanced): Change this to the name of your remote repo if you
-# cloned bash-it with a remote other than origin such as `bash-it`.
-# export BASH_IT_REMOTE="bash-it"
+export BASH_IT_THEME='$yourTheme'
 
 # Your place for hosting Git repos. I use this for private repos.
-export GIT_HOSTING="git@git.domain.com"
+export GIT_HOSTING='$yourGitRepoPriv'
 
 # Dont check mail when opening terminal.
 unset MAILCHECK
 
 # Change this to your console based IRC client of choice.
-export IRC_CLIENT="irssi"
+export IRC_CLIENT='$yourIRC'
 
 # Set this to the command you use for todo.txt-cli
 export TODO="t"
@@ -35,139 +41,37 @@ export TODO="t"
 # Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
 
-# Set Xterm/screen/Tmux title with only a short hostname.
-# Uncomment this (or set SHORT_HOSTNAME to something else),
-# Will otherwise fall back on $HOSTNAME.
-#export SHORT_HOSTNAME=$(hostname -s)
-
-# Set Xterm/screen/Tmux title with only a short username.
-# Uncomment this (or set SHORT_USER to something else),
-# Will otherwise fall back on $USER.
-#export SHORT_USER=${USER:0:8}
-
-# Set Xterm/screen/Tmux title with shortened command and directory.
-# Uncomment this to set.
-#export SHORT_TERM_LINE=true
-
-# Set vcprompt executable path for scm advance info in prompt (demula theme)
-# https://github.com/djl/vcprompt
-#export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
-
-# (Advanced): Uncomment this to make Bash-it reload itself automatically
-# after enabling or disabling aliases, plugins, and completions.
-# export BASH_IT_AUTOMATIC_RELOAD_AFTER_CONFIG_CHANGE=1
-
-# Uncomment this to make Bash-it create alias reload.
-# export BASH_IT_RELOAD_LEGACY=1
-
-# Load Bash It
 source "$BASH_IT"/bash_it.sh
 ';
 
-dnf install wget -y;
+#por ser um processo mais rapido a instalação do bash vem antes da instalação dos pacotes
+echo 'Instalando Tema no bash';
+git clone https://github.com/Bash-it/bash-it.git /home/$iAm/.bash-it/;
+chmod +x /home/$iAm/.bash-it/install.sh;
+echo 'y' | sh /home/$iAm/.bash-it/install.sh;
+echo $bashrc >> /home/$iAm/.bashrc ;
+sudo cp /home/$iAm/.bashrc /root/.bashrc ;
 
-#add repos
-echo 'Adicionando repositorios...';
-#postgress
-dnf install -yq https://download.postgresql.org/pub/repos/yum/reporpms/F-31-x86_64/pgdg-fedora-repo-latest.noarch.rpm;
-
-#rpm fusion
-dnf install -yq https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm;
-
-
-#sublime text
-rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-
-#vscode
-rpm --import https://packages.microsoft.com/keys/microsoft.asc;
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo';
-
-#nvidia(negative 17)
-dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo;
-
-dnf upgrade -yq;
-
-#add flatpak repo
-sudo dnf install -yq flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
-
-#install programs
-
-#nivida driver
-echo 'Instalando drivers Nvidia';
-dnf install -yq nvidia-driver nvidia-driver-libs.i686 nvidia-settings akmod-nvidia cuda nvidia-driver-cuda --allowerasing --best ;
-
-echo 'Instalando drivers PostgreSQL Server 12';
-dnf groupinstall -yq 'PostgreSQL Database Server 10 PGDG' --with-optional;
-
-echo 'Instalando programas via dnf';
-dnf install -yq telegram-desktop code postgresql-server postgresql-contrib java-1.8.0-openjdk java-11-openjdk stacer nano htop fira-code-fonts flat-remix-theme flat-remix-*-theme system-config-language sublime-text numlockx krita pgadmin3 vlc* gimp blender npm golang steam*;
-
-echo 'Startando o postgres';
-systemctl enable postgresql;
-systemctl start postgresql;
-postgresql-setup --initdb --unit postgresql;
-
-echo 'Instalando programas via npm';
-npm i -g npm;
-npm i -g yarn;
-
-echo 'Instalando programas via flatpak';
-flatpak install -y flathub com.spotify.Client --noninteractive;
-
-flatpak install -y flathub org.DolphinEmu.dolphin-emu --noninteractive;
-
-flatpak install -y flathub com.discordapp.Discord --noninteractive;
-
-flatpak install -y flathub com.wps.Office --noninteractive;
-
-flatpak install -y flathub com.google.AndroidStudio --noninteractive;
-
-flatpak install -y flathub io.dbeaver.DBeaverCommunity --noninteractive;
-flatpak install -y flathub io.dbeaver.DBeaverCommunity.Client.pgsql --noninteractive;
-
-echo 'Iniciando download de Google Chrome';
-wget -c https://mega.nz/linux/MEGAsync/Fedora_$(rpm -E %fedora)/x86_64/megasync-Fedora_$(rpm -E %fedora).x86_64.rpm ;
-
-wget -c https://mega.nz/linux/MEGAsync/Fedora_$(rpm -E %fedora)/x86_64/nautilus-megasync-Fedora_$(rpm -E %fedora).x86_64.rpm;
-
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm;
-
-#wget -c https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm;
-
-echo 'Instalando Mega e Google Chrome... ';
-dnf install -yq megasync-Fedora_$(rpm -E %fedora).x86_64.rpm nemo-megasync-Fedora_$(rpm -E %fedora).x86_64.rpm google-chrome-stable_current_x86_64.rpm; # dbeaver-ce-latest-stable.x86_64.rpm;
-
-
-echo 'Instalando Tema Terminal';
-git clone https://github.com/Bash-it/bash-it.git /home/$USER/.bash-it/;
-chmod +x /home/$USER/.bash-it/install.sh;
-echo 'y' | sh /home/$USER/.bash-it/install.sh;
-echo $bashrc >> /home/$USER/.bashrc ;
-cp /home/$USER/.bashrc /root/.bashrc ;
+sudo ./installer.sh;
 
 echo 'Configurando VS Code';
-su -l $USER
 code --install-extension teabyii.ayu ;
 code --install-extension dbaeumer.vscode-eslint ;
 code --install-extension esbenp.prettier-vscode ;
 code --install-extension ms-python.python;
 code --install-extension vscode-icons-team.vscode-icons;
+code --install-extension naumovs.color-highlight;
 
 echo '{ 
 	"workbench.iconTheme": "vscode-icons",
 	 "workbench.colorTheme": "Ayu Mirage",
 	  "editor.fontFamily": "Fira Code Retina",
 	  "editor.formatOnType": true,
+	  "explorer.compactFolders": false,
 	  "editor.fontLigatures": true,
 	  "explorer.confirmDelete": false,
 	  "editor.formatOnSave": true,
 	  "prettier.eslintIntegration": true,
-	  "editor.defaultFormatter": "esbenp.prettier-vscode",
+	  "prettier.singleQuote": true,
 	  "[javascript]": {"editor.defaultFormatter": "esbenp.prettier-vscode"},
-	  }' > /home/$USER/.config/Code/User/settings.json;
-
-echo 'Fim do Script Inicializando Fedora';
-echo 'Lembre de configurar as permissões do postgres -> README: Documentação de configuração do Fedora';
-echo 'Bye e bom uso :D';
+	  }' > /home/$iAm/.config/Code/User/settings.json;
